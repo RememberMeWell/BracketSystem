3302089b1fe58a655a9861ca57dc4c2dade3153f
RememberMeWell <thejbguitaryt@gmail.com>
2015-05-10 19:16:57 +0200

    boe

--- BracketSystem/WinForms/Main.cs
+++ BracketSystem/WinForms/Main.cs
@@ -15,6 +15,47 @@ namespace BracketSystem
         public Main()
         {
             InitializeComponent();
-        }     
+
+            string[] playerDatabase = new string[4] { "Hermen","Jiri", "Yolo", "Dingen" };           
+            // id naam ip adress gamesplayed
+            // 0 = Hermen
+            
+            
+
+            string players = "0;1;2;3";
+
+            List<string> players2 = new List<string>();
+            players2 = players.Split(';').ToList(); //string split => string[] => list<string<>
+
+            for (int i = 0; i < players2.Count; i++)
+            {
+                MessageBox.Show(playerDatabase[i]);
+            }
+
+            
+
+            
+
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+     
     }
 }
